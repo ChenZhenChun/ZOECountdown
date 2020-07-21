a1eacc69e905665e99b29e3ac596d97f82193957
chenzhenchun <346891964@qq.com>
2020-07-21 16:47:51 +0800

    添加正向计时

--- ZOECountdown/ZOECountdown/ZOECountdown.h
+++ ZOECountdown/ZOECountdown/ZOECountdown.h
@@ -32,6 +32,30 @@ typedef enum : NSUInteger {
                      timerBlock:(void (^)(NSString *remainTime))countdowning
                    endCountdown:(void (^)(void))endCountdown;
 
+/// 倒计时
+/// @param timeOut 倒计时秒数
+/// @param interval 倒计时间隔默认是1秒执行一次
+/// @param countdownMode 倒计时显示的模式
+/// @param countdowning 倒计时进行中block
+/// @param endCountdown 倒计时结束block
+- (instancetype)initWithTimeOut:(NSInteger)timeOut
+                       interval:(CGFloat)interval
+                  countdownMode:(CountdownMode)countdownMode
+                     timerBlock:(void (^)(NSString *remainTime))countdowning
+                   endCountdown:(void (^)(void))endCountdown;
+
+/// 计时（顺着累加计时）
+/// @param time 初始时间单位秒
+/// @param interval 执行的时间间隔，默认1秒
+/// @param countdownMode 显示的模式
+/// @param countdowning 进行中block
+/// @param endCountdown 结束block
+- (instancetype)initWithTime:(NSInteger)time
+                    interval:(CGFloat)interval
+               countdownMode:(CountdownMode)countdownMode
+                  timerBlock:(void (^)(NSString *remainTime))countdowning
+                endCountdown:(void (^)(void))endCountdown;
+
 /// 取消倒计时队列
 - (void)removeCountdown;
 
--- ZOECountdown/ZOECountdown/ZOECountdown.m
+++ ZOECountdown/ZOECountdown/ZOECountdown.m
@@ -11,10 +11,14 @@
 @interface ZOECountdown()
 @property (nonatomic, strong) dispatch_source_t timer;
 @property (nonatomic, assign) NSInteger timeout;
+@property (nonatomic,assign) CGFloat interval;//间隔
 @property (nonatomic)         void(^Mycountdowning)(NSString *remainTime);
 @property (nonatomic)         void(^MyEndcountdown)(void);
 @property (nonatomic,assign)  NSInteger countdownMode;
 @property (nonatomic, assign) NSTimeInterval timestamp;
+
+@property (nonatomic,assign) BOOL isSuspend;//timer是否挂起。dispatch_suspend 之后的Timer，是不能被释放的(timer=nil会导致崩溃)
+@property (nonatomic,assign) BOOL isASC;//是否升序计数
 @end
 
 @implementation ZOECountdown
@@ -24,9 +28,42 @@
                   countdownMode:(CountdownMode)countdownMode
                      timerBlock:(void (^)(NSString *remainTime))countdowning
                    endCountdown:(void (^)(void))endCountdown {
+    
+    return [self initWithTimeOut:timeOut
+                        interval:1
+                   countdownMode:countdownMode
+                      timerBlock:countdowning
+                    endCountdown:endCountdown];
+}
+
+- (instancetype)initWithTimeOut:(NSInteger)timeOut
+                       interval:(CGFloat)interval
+                  countdownMode:(CountdownMode)countdownMode
+                     timerBlock:(void (^)(NSString *))countdowning
+                   endCountdown:(void (^)(void))endCountdown {
     self = [super init];
     if (self) {
         _timeout = timeOut;
+        _interval = interval;
+        _Mycountdowning = countdowning;
+        _MyEndcountdown = endCountdown;
+        _countdownMode  = countdownMode;
+        [self observeApplicationActionNotification];
+        [self startTimer];
+    }
+    return self;
+}
+
+- (instancetype)initWithTime:(NSInteger)time
+                    interval:(CGFloat)interval
+               countdownMode:(CountdownMode)countdownMode
+                  timerBlock:(void (^)(NSString *))countdowning
+                endCountdown:(void (^)(void))endCountdown {
+    self = [super init];
+    if (self) {
+        _isASC = YES;
+        _timeout = time;
+        _interval = interval;
         _Mycountdowning = countdowning;
         _MyEndcountdown = endCountdown;
         _countdownMode  = countdownMode;
@@ -41,43 +78,84 @@
         __block NSString *time;
         dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
         _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
-        dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
+        if (self.interval<=0) {
+            self.interval = 1;
+        }
+        dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),self.interval*NSEC_PER_SEC, 0);
         __weak typeof(self) weakSelf = self;
         dispatch_source_set_event_handler(_timer, ^{
             __strong typeof(weakSelf) self = weakSelf;
-            if(self.timeout<0) { //倒计时结束，关闭
-                [self endTimer];
-            } else {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    switch (self.countdownMode) {
-                        case CountdownMode_HH_mm_ss:
-                            time = [NSString stringWithFormat:@"%02ld:%02ld:%02ld",(long)self.timeout/3600,(long)self.timeout%3600/60,(long)self.timeout%60];
-                            break;
-                        case CountdownMode_mm_ss:
-                            time = [NSString stringWithFormat:@"%02ld:%02ld",(long)self.timeout/60,(long)self.timeout%60];
-                            break;
-                        case CountdownMode_sss:
-                            time = [NSString stringWithFormat:@"%03ld",(long)self.timeout];
-                            break;
-                        case CountdownMode_ss:
-                            time = [NSString stringWithFormat:@"%02ld",(long)self.timeout];
-                            break;
-                        case CountdownMode_longS:
-                            time = [NSString stringWithFormat:@"%ld",(long)self.timeout];
-                        break;
-                        default:
-                            break;
+            if (self.isASC) {
+                if (self.Mycountdowning) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        switch (self.countdownMode) {
+                            case CountdownMode_HH_mm_ss:
+                                time = [NSString stringWithFormat:@"%02ld:%02ld:%02ld",(long)self.timeout/3600,(long)self.timeout%3600/60,(long)self.timeout%60];
+                                break;
+                            case CountdownMode_mm_ss:
+                                time = [NSString stringWithFormat:@"%02ld:%02ld",(long)self.timeout/60,(long)self.timeout%60];
+                                break;
+                            case CountdownMode_sss:
+                                time = [NSString stringWithFormat:@"%03ld",(long)self.timeout];
+                                break;
+                            case CountdownMode_ss:
+                                time = [NSString stringWithFormat:@"%02ld",(long)self.timeout];
+                                break;
+                            case CountdownMode_longS:
+                                time = [NSString stringWithFormat:@"%ld",(long)self.timeout];
+                                break;
+                            default:
+                                break;
+                        }
+                        self.Mycountdowning(time);
+                        self.timeout++;
+                    });
+                }else {
+                    self.timeout++;
+                }
+                
+            }else {
+                if(self.timeout<0) { //倒计时结束，关闭
+                    [self endTimer];
+                } else {
+                    if (self.Mycountdowning) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            switch (self.countdownMode) {
+                                case CountdownMode_HH_mm_ss:
+                                    time = [NSString stringWithFormat:@"%02ld:%02ld:%02ld",(long)self.timeout/3600,(long)self.timeout%3600/60,(long)self.timeout%60];
+                                    break;
+                                case CountdownMode_mm_ss:
+                                    time = [NSString stringWithFormat:@"%02ld:%02ld",(long)self.timeout/60,(long)self.timeout%60];
+                                    break;
+                                case CountdownMode_sss:
+                                    time = [NSString stringWithFormat:@"%03ld",(long)self.timeout];
+                                    break;
+                                case CountdownMode_ss:
+                                    time = [NSString stringWithFormat:@"%02ld",(long)self.timeout];
+                                    break;
+                                case CountdownMode_longS:
+                                    time = [NSString stringWithFormat:@"%ld",(long)self.timeout];
+                                break;
+                                default:
+                                    break;
+                            }
+                            self.Mycountdowning(time);
+                            self.timeout--;
+                        });
+                    }else {
+                        self.timeout--;
                     }
-                    self.Mycountdowning(time);
-                    self.timeout--;
-                });
+                    
+                }
             }
+            
         });
     }
     return _timer;
 }
 
 - (void)startTimer {
+    self.isSuspend = NO;
     dispatch_resume(self.timer);
 }
 
@@ -86,7 +164,9 @@
     __weak typeof(self) weakSelf = self;
     dispatch_async(dispatch_get_main_queue(), ^{
         __strong typeof(weakSelf) self = weakSelf;
-        self.MyEndcountdown();
+        if (self.MyEndcountdown) {
+            self.MyEndcountdown();
+        }
     });
 }
 
@@ -99,6 +179,7 @@
 - (void)applicationDidEnterBackground {
     if (self.timeout>=0) {
         self.timestamp = [NSDate date].timeIntervalSince1970+1;
+        self.isSuspend = YES;
         dispatch_suspend(_timer);
     }
 }
@@ -119,6 +200,7 @@
 }
     
 - (void)removeCountdown {
+    if (self.isSuspend) [self startTimer];
     if (_timer){
         if (@available(iOS 8.0, *)) {
             dispatch_cancel(_timer);
